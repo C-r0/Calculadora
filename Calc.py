b678f8c62a2f70fed8c181d308101695658d4103
C-r0 <146779565+C-r0@users.noreply.github.com>
2026-04-06 10:37:44 -0300

    used elif and limit to numC < 1 and remove the comments //

--- Calc.py
+++ Calc.py
@@ -1,29 +1,29 @@
-// Pergunta ao usuario a opção que deseja e armazena na variavel numC
-numC = int(input("1: Somar 2: Subtrair 3: Multiplicação 4: Divisão "))
-// Verifica se numC é maior que 4 assim a opção sendo invalida
-if numC > 4:
+# Pergunta ao usuario a opção que deseja e armazena na variavel numC
+numC = int(input("1: Somar \n2: Subtrair \n3: Multiplicação \n4: Divisão \n"))
+# Verifica se numC é maior que 4 assim a opção sendo invalida
+if numC > 4 or numC < 1:
     print("Coloque uma opção valida.")
     quit()
-// Variavel num1 onde tem o primeiro numero
+# Variavel num1 onde tem o primeiro numero
 num1 = int(input("Primeiro Numero: "))
-// Variavel num2 onde tem o segundo numero
+# Variavel num2 onde tem o segundo numero
 num2 = int(input("Segundo Numero: "))
-// Verifica se numC é 1 assim somando os valores
+# Verifica se numC é 1 assim somando os valores
 if numC == 1:
     Resultado = num1 + num2
     print("A Resposta de Sua Soma é:", Resultado)
-// Verifica se numC é 2 assim subtraindo os valores
-if numC == 2:
+# Verifica se numC é 2 assim subtraindo os valores
+elif numC == 2:
     Resultado = num1 - num2
     print("A Resposta de Sua Subtração é:", Resultado)
-// Verifica se numC é 3 assim multiplicando os valores
-if numC == 3:
+# Verifica se numC é 3 assim multiplicando os valores
+elif numC == 3:
     Resultado = num1 / num2
     print("A Resposta de Sua Multiplicação é:", Resultado)
-// Verifica se numC é 4 assim dividindo os valores
-if numC == 4:
+# Verifica se numC é 4 assim dividindo os valores
+elif numC == 4:
     Resultado = num1 * num2
     print("A Resposta de Sua Divisão é:", Resultado)
     
-// Fiz para não fechar o cmd ao abrir
+# Fiz para não fechar o cmd ao abrir
 input()
